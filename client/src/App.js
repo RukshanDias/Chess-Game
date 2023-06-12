55f8e5464c3926c0efb2395dd8a68d51384c9802
RukshanDias <rukshandias002@gmail.com>
2023-06-12 19:16:48 +0530

    basic chessboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from 'react';
+import {Chessboard} from 'react-chessboard'
+import {Chess} from 'chess.js'
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  const [game, setGame] = useState(new Chess());
+    return (
+        <div className="App">
+          <Chessboard/>
+            {/* <Chessboard position={game.fen()} onPieceDrop={onDrop} /> */}
+        </div>
+    );
 }
 
 export default App;
