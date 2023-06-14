e7cf95e07d4cd8ba5e75a6e52f42801f3f9d20dd
RukshanDias <rukshandias002@gmail.com>
2023-06-14 16:06:07 +0530

    display possible moves on hover

--- client/src/components/ChessGame.js
+++ client/src/components/ChessGame.js
@@ -8,8 +8,11 @@ const ChessGame = () => {
     const [game, setGame] = useState(new Chess());
     console.log(game);
     const [currentTimeout, setCurrentTimeout] = useState(undefined);
+    const [optionSquare, setOptionSquares] = useState({});
 
     // effects
+
+    // Functions & Methods
 
     const safeGameMutate = (modify) => {
         setGame((g) => {
@@ -37,17 +40,29 @@ const ChessGame = () => {
         }
 
         // store timeout so it can be cleared on undo/reset so computer doesn't execute move
-        // makeRandomMove();
         const newTimeout = setTimeout(makeRandomMove, 200);
         setCurrentTimeout(newTimeout);
         return true;
     }
 
+    const isGameOver = () => {
+        console.log("checking game")
+        if (game.game_over()) {
+            alert("game over");
+            return true;
+        } else if (game.in_draw()) {
+            alert("draw");
+            return true;
+        }
+        return false;
+    };
+
     function makeRandomMove() {
         const possibleMoves = game.moves();
         console.log(possibleMoves);
         // exit if the game is over
-        if (game.game_over() || game.in_draw() || possibleMoves.length === 0) return;
+        // if (game.game_over() || game.in_draw() || possibleMoves.length === 0) return;
+        if(isGameOver())return;
 
         const randomIndex = Math.floor(Math.random() * possibleMoves.length);
         console.log("random no:" + randomIndex);
@@ -56,10 +71,45 @@ const ChessGame = () => {
         });
     }
 
+    const onMouseOverSquare = (square) => {
+        // alert("hover" + square);
+        const moves = game.moves({
+            square,
+            verbose: true,
+        });
+        console.log(moves);
+        if (moves.length === 0) {
+            return;
+        }
+
+        const newSquares = {};
+        moves.map((move) => {
+            newSquares[move.to] = {
+                background:
+                    game.get(move.to) && game.get(move.to).color !== game.get(square).color
+                        ? "radial-gradient(circle, rgba(0,0,0,.1) 85%, transparent 85%)"
+                        : "radial-gradient(circle, rgba(0,0,0,.1) 25%, transparent 25%)",
+                borderRadius: "50%",
+            };
+            return move;
+        });
+        newSquares[square] = {
+            background: "rgba(255, 255, 0, 0.4)",
+        };
+        console.log(newSquares);
+        setOptionSquares(newSquares);
+    };
+
     return (
         <div className="App">
             {/* <Chessboard/> */}
-            <Chessboard position={game.fen()} onPieceDrop={onDrop}/>
+            <Chessboard
+                position={game.fen()}
+                onPieceDrop={onDrop}
+                customSquareStyles={{ ...optionSquare }}
+                onMouseOverSquare={onMouseOverSquare}
+                onMouseOutSquare={() => setOptionSquares({})}
+            />
         </div>
     );
 };
