67e1734b2207abfd3d7449a66dbe476e6d3ee623
RukshanDias <rukshandias002@gmail.com>
2023-06-20 22:47:22 +0530

    apply react router dom to the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
 import ChessGame from "./components/ChessGame";
+import LoginRegister from "./pages/LoginRegister/LoginRegister";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
     return (
         <div className="App">
-          {/* <Chessboard/> */}
-            {/* <Chessboard position={game.fen()} onPieceDrop={onDrop} /> */}
-            <ChessGame/>
+            <Router>
+                <Routes>
+                <Route path="/" element={<ChessGame />} />
+                <Route path="/login-register" element={<LoginRegister />} />
+                </Routes>
+            </Router>
         </div>
     );
 }
