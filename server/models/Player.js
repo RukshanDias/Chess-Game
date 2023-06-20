3377229bcf0cb64b449860ef0ca18ce4426b3978
RukshanDias <rukshandias002@gmail.com>
2023-06-20 22:45:18 +0530

    update Player model

--- server/models/Player.js
+++ server/models/Player.js
@@ -3,26 +3,31 @@ const playerSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
     },
     matches: {
         type: Number,
-        required:true
+        default: 0,
     },
     won: {
         type: Number,
-        required:true
+        default: 0,
     },
     lost: {
         type: Number,
-        required:true
+        default: 0,
     },
     draw: {
         type: Number,
-        required:true
+        default: 0,
     },
     points: {
         type: Number,
-        required:true
+        default: 0,
     },
 });
 module.exports = mongoose.model("Player", playerSchema);
