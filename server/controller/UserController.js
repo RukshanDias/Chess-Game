ca93eda2b0dbbd90f5a2bbc73c88f8fc5c3c3535
RukshanDias <rukshandias002@gmail.com>
2023-06-21 20:55:50 +0530

    login, register & session controller

--- server/controller/UserController.js
+++ server/controller/UserController.js
@@ -2,30 +2,49 @@ const User = require("../models/Player");
 
 // login
 const loginUser = async (req, res) => {
-    res.json({ msg: "login" });
+    console.log("login called");
+    if (req.body.username && req.body.password) {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username: username, password: password });
+
+        if (user) {
+            req.session.user = user;
+            req.session.authorized = true;
+            req.session.save();
+            res.send({ username: username });
+        } else {
+            res.status(500).send("error");
+        }
+    }
 };
 
 // register
 const registerUser = async (req, res) => {
-    console.log("in controller");
-    console.log(req.body);
     const user = new User({
         username: req.body.username,
         password: req.body.password,
     });
     try {
-        console.log("in try");
         await user.save();
-        console.log("data saved");
-        // res.redirect("/");
+        req.session.username = req.body.username;
+        res.send({ success: true, username: req.body.username });
     } catch (error) {
         if (error.code === 11000 && error.keyPattern && error.keyValue) {
             console.log("already registered");
+            res.send({ success: false });
         } else {
             res.status(500).send("error");
-            // res.redirect("/");
         }
     }
 };
 
-module.exports = { loginUser, registerUser };
+//session
+const getSession = (req, res) => {
+    console.log(req.session);
+    // console.log("session set 3 ->", req.session.user.username);
+    const username = req.session;
+    console.log("server name->", username);
+    res.json({ username });
+};
+
+module.exports = { loginUser, registerUser, getSession };
