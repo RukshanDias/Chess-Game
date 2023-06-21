8420facb66cf14e02f93d9131238dcb18c023397
RukshanDias <rukshandias002@gmail.com>
2023-06-21 20:10:30 +0530

    created session router

--- server/Routes/Player.js
+++ server/Routes/Player.js
@@ -2,12 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 // controllers
-const { loginUser, registerUser } = require("../controller/UserController");
+const { loginUser, registerUser, getSession } = require("../controller/UserController");
 
 // login
 router.post("/login", loginUser);
 
 // register
 router.post("/register", registerUser);
+
+// sessions
+router.get("/session", getSession);
 
 module.exports = router;
